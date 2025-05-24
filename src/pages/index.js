c19b2535b5b394b8dedcf4a8fd8d165034b65efb
NikEmman <121699424+NikEmman@users.noreply.github.com>
2025-05-24 21:51:25 +0300

    Deleted boilerplate stuff

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Hogwarts Wizard&apos;s School</title>
+        <meta name="description" content="Hogwarts Wizard's School" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/harryPotter.webp" />
+      </Head>
+
+      <main></main>
+    </>
+  );
+}
